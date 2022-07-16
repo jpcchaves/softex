1cfb3026db7570dc9f9031f016cb464adfff9c4f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-15 23:23:42 -0300

    finished function

--- desafio_insertionSort/desafio.js
+++ desafio_insertionSort/desafio.js
@@ -1,32 +1,50 @@
-// gera um array com 30 números
-const geraArray = () => {
-  const arrayInicial = [];
-  for (let i = 1; i < 60; i++) {
-    let newNumber = i;
-    arrayInicial.push(newNumber);
+// Na função abaixo, vemos que temos várias funções que interagem entre si.
+// Primeiro, temos a função geraArray, que vai nos dar um array com 30 elementos, todos ímpares.
+// Segundo, temos a função suffleArray, que vai receber nosso array como parâmetro e vai embaralhar os números
+// Terceiro, temos a função ordenateArray, que vai ordenar nosso array novamente, que é passado como retorno da função desafioSoftex()
+
+function desafioSoftex() {
+  // gera um array com 60 números
+  const geraArray = () => {
+    let arrayInicial = [];
+    for (let i = 1; i < 60; i++) {
+      if (i % 2 == 1) {
+        arrayInicial.push(i);
+      }
+    }
+    return arrayInicial;
+  };
+  const arrayGerado = geraArray();
+
+  function shuffleArray(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
   }
-  return arrayInicial;
-};
-// Randomize the array
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
+
+  // randomArray
+  const randomArray = shuffleArray(arrayGerado);
+
+  // ordenate array
+  const ordenateArray = (randomArray) => {
+    return randomArray.sort((a, b) => a - b);
+  };
+
+  let finalArray = ordenateArray(randomArray);
+  return finalArray;
 }
-// randomArray
-let randomArray = shuffleArray(geraArray());
-
-// Only odds
-const onlyOdds = (num) => num % 2 != 0;
-
-// filterArray
-const filterArray = (randomArr) => {
-  let newArray = randomArr.filter((number) => onlyOdds(number));
-  return newArray;
-};
-
-console.log(filterArray(randomArray));
+console.log(desafioSoftex());
