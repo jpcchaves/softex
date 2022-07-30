b27163c130a20751e02e90220e641c4a1f896697
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-30 15:26:28 -0300

    resolução desafio

---
+++ class_poo2/desafio.js
@@ -0,0 +1,31 @@
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  get nomeDaPessoa() {
+    return this.nome;
+  }
+
+  get sobrenomeDaPessoa() {
+    return this.sobrenome;
+  }
+
+  set mudarNomePessoa(nomePessoa) {
+    return (this.nome = nomePessoa);
+  }
+
+  set mudarSobrenomePessoa(sobrenomePessoa) {
+    return (this.sobrenome = sobrenomePessoa);
+  }
+}
+
+let pessoaExemplo = new Pessoa("João", "Paulo");
+console.log(pessoaExemplo.nome);
+console.log(pessoaExemplo.sobrenome);
+
+pessoaExemplo.mudarNomePessoa = "Zezinho";
+pessoaExemplo.mudarSobrenomePessoa = "Shaolin";
+console.log(pessoaExemplo.nome);
+console.log(pessoaExemplo.sobrenome);
